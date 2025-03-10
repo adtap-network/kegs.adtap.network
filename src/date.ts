92ccfcb26f2b327d5450eadb7e0f63b0cb5d08a6
Rob Panico <github@adtap.network>
2025-03-10 01:40:15 -0400

    1.2.0.

---
+++ src/date.ts
@@ -0,0 +1,24 @@
+class ExpandedDate extends Date {
+    getRealDay() { return this.getDay() + 1; }
+
+    getRealMonth() { return this.getMonth() + 1; }
+
+    getShortHours() {
+        let h = this.getHours();
+        let t = 'am';
+        let s = h;
+        if (h === 0) { s = 12; }
+        else if (h === 12) { t = 'pm'; }
+        else if (h > 12) { t = 'pm'; s = h - 12; }
+        return `${s} ${t}`;
+    }
+
+    getMonthAsString() { const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; return months[this.getMonth()]; }
+
+    getDayAsString() { const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; return days[this.getDay()]; }
+
+    setRealDay(v: number) { this.setDate(v); }
+
+    setRealMonth(v: number) { this.setMonth(v - 1); }
+}
+export default ExpandedDate;
